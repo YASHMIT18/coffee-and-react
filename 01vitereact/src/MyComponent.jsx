c6e72309eb248e44acf8667a0c0bcb054238dd0c
Yashmit Lamba <lambayashmit@gmail.com>
2025-10-02 13:23:40 +0530

    adding my project

---
+++ 01vitereact/src/MyComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MyComponent() {
+  return (
+    <div>
+      <h2>Hello from MyComponent!</h2>
+      <p>This is a sample React component.</p>
+    </div>
+  );
+}
