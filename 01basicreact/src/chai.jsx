55fd952ee97313f7a710335d9be9fcd43278e59f
Yashmit Lamba <lambayashmit@gmail.com>
2025-10-03 12:51:52 +0530

    started react first project

--- 01basicreact/src/chai.jsx
+++ 01basicreact/src/chai.jsx
@@ -2,7 +2,7 @@ function Chai(){
 
     return(
 
-        <h1>hello chai is ready</h1>
+        <h1>welcome</h1>
     )
 }
 
--- 01vitereact/src/chai.jsx
+++ 01vitereact/src/chai.jsx
@@ -1,7 +1,9 @@
 function Chai() {
     return(
         
-     <a href="https://www.youtube.com/"> <div className="chai-container"> <h1>hello their is chai</h1></div></a> 
+     <a href="https://www.youtube.com/"> <div className="chai-container">
+         <h1>Hello you are starting react with yashmit</h1>
+         </div></a> 
     )
 }
 
