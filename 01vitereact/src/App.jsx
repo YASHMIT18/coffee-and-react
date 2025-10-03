949d5bdebd2b430936db663d26b952c52d5f7195
Yashmit Lamba <lambayashmit@gmail.com>
2025-10-03 12:37:10 +0530

    how to export and use of js and jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,29 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import MyComponent from './MyComponent'
+import React from "react";
+import "./App.css";
+import Chai from "./chai.jsx";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-<h6>  hiii</h6>       
-
-
-      <h1>My first project with react | YASHMIT LAMBA</h1>
-      
-     </div>
-     <div>
-      <MyComponent/>
-
-     </div>
-    </>
-  )
+    <Chai />
+  );
 }
-
-export default App
-
-
+ 
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,8 @@
+function Chai() {
+    return(
+        
+     <a href="https://www.youtube.com/"> <div className="chai-container"> <h1>hello their is chai</h1></div></a> 
+    )
+}
+
+export default Chai;
